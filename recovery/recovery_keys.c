df953ee5d08c3acc5af279ccca6ea81907a19fea
Boris Popov <bvpopov@gmail.com>
2013-02-20 13:50:29 -0800

    Recovery keys update

--- recovery/recovery_keys.c
+++ recovery/recovery_keys.c
@@ -1,42 +1,9 @@
-/*
- * Copyright (C) 2009 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 #include <linux/input.h>
 
 #include "recovery_ui.h"
 #include "common.h"
 #include "extendedcommands.h"
 
-char* MENU_HEADERS[] = { "Use volume to highlight; home to select.",
-                         "",
-                         NULL };
-
-char* MENU_ITEMS[] = { "reboot system now",
-                       "apply sdcard:update.zip",
-                       "wipe data/factory reset",
-                       "wipe cache partition",
-                       "install zip from sdcard",
-                       "backup and restore",
-                       "mounts and storage",
-                       "advanced",
-                       NULL };
-
-int device_recovery_start() {
-    return 0;
-}
 
 int device_toggle_display(volatile char* key_pressed, int key_code) {
     int alt = key_pressed[KEY_LEFTALT] || key_pressed[KEY_RIGHTALT];
@@ -44,13 +11,10 @@ int device_toggle_display(volatile char* key_pressed, int key_code) {
         return 1;
     // allow toggling of the display if the correct key is pressed, and the display toggle is allowed or the display is currently off
     if (ui_get_showing_back_button()) {
-        return get_allow_toggle_display() && (key_code == KEY_MENU || key_code == KEY_END);
+        return 0;
+        //return get_allow_toggle_display() && (key_code == KEY_HOME || key_code == KEY_MENU || key_code == KEY_END);
     }
-    return get_allow_toggle_display() && (key_code == KEY_MENU || key_code == KEY_POWER || key_code == KEY_END);
-}
-
-int device_reboot_now(volatile char* key_pressed, int key_code) {
-    return 0;
+    return get_allow_toggle_display() && (key_code == KEY_HOME || key_code == KEY_MENU || key_code == KEY_POWER || key_code == KEY_END);
 }
 
 int device_handle_key(int key_code, int visible) {
@@ -58,25 +22,26 @@ int device_handle_key(int key_code, int visible) {
         switch (key_code) {
             case KEY_CAPSLOCK:
             case KEY_DOWN:
-            case KEY_VOLUMEUP:
+            case KEY_VOLUMEDOWN:
+            case KEY_MENU:
                 return HIGHLIGHT_DOWN;
 
             case KEY_LEFTSHIFT:
             case KEY_UP:
-            case KEY_VOLUMEDOWN:
+            case KEY_VOLUMEUP:
                 return HIGHLIGHT_UP;
 
             case KEY_POWER:
                 if (ui_get_showing_back_button()) {
                     return SELECT_ITEM;
                 }
-                if (!get_allow_toggle_display())
+                if (!get_allow_toggle_display() && !ui_root_menu) {
                     return GO_BACK;
+                }
                 break;
             case KEY_LEFTBRACE:
             case KEY_ENTER:
             case BTN_MOUSE:
-            case KEY_CENTER:
             case KEY_CAMERA:
             case KEY_F21:
             case KEY_SEND:
@@ -85,19 +50,19 @@ int device_handle_key(int key_code, int visible) {
             
             case KEY_END:
             case KEY_BACKSPACE:
-            case KEY_BACK:
-                if (!get_allow_toggle_display())
+            case KEY_SEARCH:
+                if (ui_get_showing_back_button()) {
+                    return SELECT_ITEM;
+                }
+                if (!get_allow_toggle_display() && !ui_root_menu) {
                     return GO_BACK;
+                }
+            case KEY_BACK:
+                if (!ui_root_menu) {
+                    return GO_BACK;
+                }
         }
     }
 
     return NO_ACTION;
-}
-
-int device_perform_action(int which) {
-    return which;
-}
-
-int device_wipe_data() {
-    return 0;
 }
